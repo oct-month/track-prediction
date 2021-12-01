c3e3f73869b3621384be983853390f4a66d0ef43
ablocker <oct-month@qq.com>
2021-12-01 20:13:45 +0800

    'bug'

--- model/v2/config.py
+++ model/v2/config.py
@@ -1,6 +1,6 @@
 from mxnet import gpu, cpu
 from mxnet.util import get_gpu_count
 
-gpu_counts = 3#get_gpu_count()
+gpu_counts = get_gpu_count()
 device = gpu() if gpu_counts > 0 else cpu()
 devices = [gpu(i) for i in range(gpu_counts)] if gpu_counts > 0 else [cpu()]
